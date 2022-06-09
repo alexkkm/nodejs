269fec004a394374c793a1a249b0fb67c2d69de2
Alexkkm <alexkong0222@gmail.com>
2022-06-09 10:21:51 +0800

    init

---
+++ async.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+
+console.log("阻塞代码实例");
+//阻塞代码实例
+var data = fs.readFileSync('input.txt');
+
+console.log(data.toString());
+console.log("程序执行结束!");
+
+
+console.log("非阻塞代码实例");
+//非阻塞代码实例
+fs.readFile('input.txt', function (err, data) {
+    if (err) return console.error(err);
+    console.log(data.toString());
+});
+
+console.log("程序执行结束!");
